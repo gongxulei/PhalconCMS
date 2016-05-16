5d5d6795912458c0297cf6a4589fcde8d4f520de
zhengwenzhong <zhengwenzhong@zens.asia>
2016-05-16 10:53:58 +0800

    前后端静态模板

--- app/frontend/controllers/IndexController.php
+++ app/frontend/controllers/IndexController.php
@@ -11,11 +11,26 @@ namespace Marser\App\Frontend\Controllers;
 
 class IndexController extends \Phalcon\Mvc\Controller{
 
-    public function testAction(){
-        $this -> view -> pick('index/test');
+    public function indexAction(){
+        $this->view->setVars(
+            array(
+                'title'         =>  'fronted_title',
+                'content'       =>  'fronted_content',
+                'description'   =>  'description',
+                'my_team'       =>  'zwz,www.marser.cn',
+                'team_desc'     =>  'fuck fuck fuck',
+                'classfy'       =>  array(
+                    'one'   =>  123,
+                    'two'   =>  456,
+                    'three' =>  789
+                ),
+            )
+        );
     }
 
-    public function notfoundAction(){
-        echo 'frontend - 404';
+    public function testAction(){
+        $this -> view -> title = 'index/test';
+        $this -> view -> description = 'fuck fuck fuck ';
+        $this -> view -> pick('index/test');
     }
 }
